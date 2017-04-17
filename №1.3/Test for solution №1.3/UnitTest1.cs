83b30d9be1d63bfb4f0e2093274ccd04a37d0f8b
Ekaterina Petrova <penspb@gmail.com>
2017-04-17 21:46:59 +0300

    1.3

---
+++ №1.3/Test for solution №1.3/UnitTest1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _1._3;
+
+namespace Test_for_solution__1._3
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        public void TestSort0()
+        {
+            int[] array = { 1, 10, -1, 4, -4, 6};
+            Program.BubbleSort(array);
+
+            bool check = true;
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] > array[i + 1])
+                {
+                    check = false;
+                }
+            }
+
+            Assert.IsTrue(check);
+        }
+
+        [TestMethod]
+        public void TestSort1()
+        {
+            int[] array = { 5, 4, 3, 2, 1, 0, -1, 6};
+
+            Program.BubbleSort(array);
+
+            Assert.AreEqual(5, array[6]);
+        }
+
+        [TestMethod]
+        public void TestSort3()
+        {
+            int[] array = { 1, 2, 3, 4, 8, 14, 16 };
+            Program.BubbleSort(array);
+
+            bool check = true;
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] > array[i + 1])
+                {
+                    check = false;
+                }
+            }
+
+            Assert.IsTrue(check);
+        }
+
+    }
+}
---
+++ №1.3/№1.3/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._3
+{
+    public class Program
+    {
+
+        public static void BubbleSort(int[] array)
+        {
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int variable = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = variable;
+                    }
+                }
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Good day! Please enter the size of array:");
+
+            int size = 0;
+            size = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[size];
+
+            Console.WriteLine("Fine! Please enter the array:");
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            BubbleSort(array);
+
+            Console.WriteLine("After all:");
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+
+        }
+
+    }
+
+}
