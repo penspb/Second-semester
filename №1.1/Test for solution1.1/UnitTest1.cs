690ee229fa3c2e2849c95971daa45311f90a5d22
Ekaterina Petrova <penspb@gmail.com>
2017-04-17 19:37:39 +0300

    1.1

---
+++ №1.1/Test for solution1.1/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _1._1;
+
+namespace Test_for_solution1._1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CheckingAnsew0()
+        {
+            Assert.AreEqual(Program.Factorial(3), 6);
+        }
+
+        [TestMethod]
+        public void CheckingAnsew1()
+        {
+            Assert.AreEqual(Program.Factorial(0), 1);
+        }
+
+        [TestMethod]
+        public void CheckingAnsew2()
+        {
+            Assert.AreEqual(Program.Factorial(7), 5040);
+        }
+    }
+}
---
+++ №1.1/№1.1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _1._1
+{
+    public class Program
+    {
+        /// <summary>
+        /// Realization of factorial
+        /// </summary>
+        /// <returns>Factorial of number</returns>
+        public static int Factorial(int number)
+        {
+            if (number < 0)
+            {
+                throw new PushException("Error. The number is negative");
+            }
+
+            if (number == 0)
+            {
+                return 1;
+            }
+
+            return number * Factorial(number - 1);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Good day! Please enter the number:");
+            int newNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine(Factorial(newNumber));
+        }
+    }
+}
