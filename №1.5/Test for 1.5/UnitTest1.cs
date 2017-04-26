13480a52cdcee8ae22e9149132a0550524065553
Ekaterina Petrova <penspb@gmail.com>
2017-04-26 23:16:15 +0300

    №1.5

---
+++ №1.5/Test for 1.5/UnitTest1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _1._5;
+
+namespace Test_for_1._5
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        public void TestForSwap()
+        {
+
+            int[,] array = { { 1, 10, 4 }, { -4, -2, 3 }, { 7, -1, 4 } };
+            Program.LineSort(array);
+
+            Assert.AreEqual(3, array[1, 1]);
+
+        }
+
+        [TestMethod]
+        public void SimpleTestForSort()
+        {
+
+            int[,] array = { { 3, 2, 1 }, { 2, 1, 0 }, { 1, 0, -1 } };
+            Program.LineSort(array);
+
+            Assert.AreEqual(1, array[0, 0]);
+
+        }
+
+        [TestMethod]
+        public void TestForSort0()
+        {
+
+            int[,] array = { { 1, 10, 4 }, { -4, -2, 3 }, { 7, -1, 4 } };
+            Program.LineSort(array);
+
+            bool check = true;
+            for (int i = 0; i < array.GetLength(1) - 1; i++)
+            {
+
+                if (array[0, i + 1] < array[0, i])
+                {
+
+                    check = false;
+
+                }
+
+            }
+
+            Assert.IsTrue(check);
+
+        }
+
+        [TestMethod]
+        public void TestForSort1()
+        {
+
+            int[,] array = { { 1, 2, 3 }, { 1, 2, 3 }, { 1, 2, 3 } };
+            Program.LineSort(array);
+
+            bool check = true;
+            for (int i = 0; i < array.GetLength(1) - 1; i++)
+            {
+
+                if (array[0, i + 1] < array[0, i])
+                {
+
+                    check = false;
+
+                }
+
+            }
+
+            Assert.IsTrue(check);
+
+        }
+
+        [TestMethod]
+        public void TestForSort2()
+        {
+
+            int[,] array = { { 1 } };
+            Program.LineSort(array);
+
+            bool check = true;
+            for (int i = 0; i < array.GetLength(1) - 1; i++)
+            {
+
+                if (array[0, i + 1] < array[0, i])
+                {
+
+                    check = false;
+
+                }
+
+            }
+
+            Assert.IsTrue(check);
+
+        }
+
+        [TestMethod]
+        public void TestForSort3()
+        {
+
+            int[,] array = { { 1, 3, 2 } };
+            Program.LineSort(array);
+
+            bool check = true;
+            for (int i = 0; i < array.GetLength(1) - 1; i++)
+            {
+
+                if (array[0, i + 1] < array[0, i])
+                {
+
+                    check = false;
+
+                }
+
+            }
+
+            Assert.IsTrue(check);
+
+        }
+
+    }
+}
---
+++ №1.5/№1.5/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+namespace _1._5
+{
+    public class Program
+    {
+        /// <summary>
+        /// Swap adjacent columns
+        /// </summary>
+        public static void SwapColumn(int[,] array, int numberOfFirstColumn)
+        {
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+
+                int zero = array[i, numberOfFirstColumn];
+                array[i, numberOfFirstColumn] = array[i, numberOfFirstColumn + 1];
+                array[i, numberOfFirstColumn + 1] = zero;
+
+            }
+        }
+
+        /// <summary>
+        /// Probable sort
+        /// </summary>s
+        public static void LineSort(int[,] array)
+        {
+
+            for (int i = 0; i < array.GetLength(1) - 1; i++)
+            {
+
+                for (int j = 0; j < array.GetLength(1) - 1 - i; j++)
+                {
+
+                    if (array[0, j + 1] < array[0, j])
+                    {
+
+                        SwapColumn(array, j);
+
+                    }
+
+                }
+
+            }
+
+        }
+
+        /// <summary>
+        /// Read array from consol
+        /// </summary>
+        static void ReadArray(int[,] array)
+        {
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+
+                    array[i, j] = Convert.ToInt32(Console.ReadLine());
+
+                }
+
+            }
+
+        }
+
+        /// <summary>
+        /// Print result of sort
+        /// </summary>
+        /// <param name="array"></param>
+        static void WriteArray(int[,] array)
+        {
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+
+                    Console.Write($"{array[i, j]} ");
+
+                }
+
+                Console.WriteLine();
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Good day! Please enter the size of array: lines, columns");
+            int line = 0;
+            line = Convert.ToInt32(Console.ReadLine());
+            int column = 0;
+            column = Convert.ToInt32(Console.ReadLine());
+
+            int[,] array = new int[line, column];
+
+            Console.WriteLine("Fine! Please enter the array:");
+            ReadArray(array);
+
+            LineSort(array);
+
+            Console.WriteLine("Result of sort:");
+            WriteArray(array);
+
+        }
+
+    }
+
+}
