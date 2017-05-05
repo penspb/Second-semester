42df0a93f24ff5ca547757da1331224d9f1cc066
Ekaterina Petrova <penspb@gmail.com>
2017-05-05 21:52:09 +0300

    Исправлен внешний вид

--- №1.5/№1.5/Program.cs
+++ №1.5/№1.5/Program.cs
@@ -2,6 +2,9 @@
 
 namespace _1._5
 {
+    /// <summary>
+    /// Project implementation. Including line sort and conversation with user
+    /// </summary>
     public class Program
     {
         /// <summary>
@@ -9,14 +12,11 @@ namespace _1._5
         /// </summary>
         public static void SwapColumn(int[,] array, int numberOfFirstColumn)
         {
-
             for (int i = 0; i < array.GetLength(0); i++)
             {
-
                 int zero = array[i, numberOfFirstColumn];
                 array[i, numberOfFirstColumn] = array[i, numberOfFirstColumn + 1];
                 array[i, numberOfFirstColumn + 1] = zero;
-
             }
         }
 
@@ -25,24 +25,16 @@ namespace _1._5
         /// </summary>s
         public static void LineSort(int[,] array)
         {
-
             for (int i = 0; i < array.GetLength(1) - 1; i++)
             {
-
                 for (int j = 0; j < array.GetLength(1) - 1 - i; j++)
                 {
-
                     if (array[0, j + 1] < array[0, j])
                     {
-
                         SwapColumn(array, j);
-
                     }
-
                 }
-
             }
-
         }
 
         /// <summary>
@@ -50,19 +42,13 @@ namespace _1._5
         /// </summary>
         static void ReadArray(int[,] array)
         {
-
             for (int i = 0; i < array.GetLength(0); i++)
             {
-
                 for (int j = 0; j < array.GetLength(1); j++)
                 {
-
                     array[i, j] = Convert.ToInt32(Console.ReadLine());
-
                 }
-
             }
-
         }
 
         /// <summary>
@@ -71,25 +57,19 @@ namespace _1._5
         /// <param name="array"></param>
         static void WriteArray(int[,] array)
         {
-
             for (int i = 0; i < array.GetLength(0); i++)
             {
-
                 for (int j = 0; j < array.GetLength(1); j++)
                 {
-
                     Console.Write($"{array[i, j]} ");
-
                 }
 
                 Console.WriteLine();
             }
-
         }
 
         static void Main(string[] args)
         {
-
             Console.WriteLine("Good day! Please enter the size of array: lines, columns");
             int line = 0;
             line = Convert.ToInt32(Console.ReadLine());
@@ -105,9 +85,6 @@ namespace _1._5
 
             Console.WriteLine("Result of sort:");
             WriteArray(array);
-
         }
-
     }
-
 }
