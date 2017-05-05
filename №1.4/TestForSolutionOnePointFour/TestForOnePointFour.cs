ff215a1ae7871e8e92e16a8c1c6afdb5aedbf958
Ekaterina Petrova <penspb@gmail.com>
2017-05-05 23:44:38 +0300

    1.4

---
+++ №1.4/TestForSolutionOnePointFour/TestForOnePointFour.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _1._4;
+
+namespace TestForSolutionOnePointFour
+{
+    [TestClass]
+    public class TestForOnePointFour
+    {
+        [TestMethod]
+        [ExpectedException(typeof(EvenNumberException))]
+        public void CheckingException()
+        {
+            int number = 2;
+            int[,] firstArray = { { 1, 2 }, { 3, 4 } };
+            int[] lastArray = new int[number * number];
+
+            Program.SpiralLine(firstArray, lastArray);
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+            int number = 1;
+            int[,] firstArray = { { 5 } };
+            int[] lastArray = new int[number * number];
+
+            Program.SpiralLine(firstArray, lastArray);
+
+            Assert.AreEqual(5, lastArray[0]);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            int number = 5;
+            int[,] firstArray = { { 1, 2, 3, 4, 5 }, { 6, 7, 8, 9, 10 }, { 11, 12, 13, 14, 15 }, { 16, 17, 18, 19, 20 }, { 21, 22, 23, 24, 25 } };
+            int[] lastArray = new int[number * number];
+
+            Program.SpiralLine(firstArray, lastArray);
+
+            Assert.AreEqual(3, lastArray[10]);
+        }
+    }
+}
---
+++ №1.4/№1.4/EvenNumberException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._4
+{
+    /// <summary>
+    /// Exception if input data are even numbers
+    /// </summary>
+    [Serializable]
+    public class EvenNumberException : Exception
+    {
+        public EvenNumberException() { }
+        public EvenNumberException(string message) : base(message) { }
+        public EvenNumberException(string message, Exception inner) : base(message, inner) { }
+        protected EvenNumberException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+    
+}
