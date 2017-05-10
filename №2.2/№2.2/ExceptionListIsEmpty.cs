954fefdafc7cb294c9025bf9f5cc6387264e3d97
Ekaterina Petrova <penspb@gmail.com>
2017-05-10 20:16:04 +0300

    №2.2

---
+++ №2.2/№2.2/ExceptionListIsEmpty.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._2
+{
+    /// <summary>
+    /// Exception: appeal to empty list
+    /// </summary>
+    [Serializable]
+    public class ExceptionListIsEmpty : Exception
+    {
+        public ExceptionListIsEmpty() { }
+        public ExceptionListIsEmpty(string message) : base(message) { }
+        public ExceptionListIsEmpty(string message, Exception inner) : base(message, inner) { }
+        protected ExceptionListIsEmpty(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
