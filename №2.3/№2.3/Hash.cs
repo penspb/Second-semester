903a96736e232c88045e38f768fa08ec2657aa41
Ekaterina Petrova <penspb@gmail.com>
2017-05-26 13:16:50 +0300

    №2.3

---
+++ №2.3/№2.3/Hash.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._3
+{
+    public class Hash
+    {
+        /// <summary>
+        /// size of table
+        /// </summary>
+        private const int sizeOfTable = 63;
+
+        /// <summary>
+        /// hash table
+        /// </summary>
+        public List[] table { get; set; }
+
+        /// <summary>
+        /// describe of hash table
+        /// </summary>
+        public Hash()
+        {
+            table = new List[sizeOfTable];
+
+            for (int i = 0; i < sizeOfTable; i++)
+            {
+                table[i] = new List();
+            }
+        }
+
+        /// <summary>
+        /// hash funcion
+        /// </summary>
+        /// <returns>result of function</returns>
+        public int Function(string workingString)
+        {
+            int result = 0;
+            for (int i = 0; i < workingString.Length; i++)
+            {
+                result = result + workingString[i] * i;
+            }
+
+            return Math.Abs(result % sizeOfTable);
+        }
+
+        /// <summary>
+        /// add sample to hash-table
+        /// </summary>
+        /// <param name="value">sample</param>
+        public void Add(string workingString)
+        {
+            table[Function(workingString)].Add(0, workingString);
+        }
+
+        /// <summary>
+        /// delete element by value
+        /// </summary>
+        public void Delete(string workingString)
+        {
+            if (!table[Function(workingString)].IsThereValue(workingString))
+            {
+                throw new ExceptionNonexistentPosition("Value is not found");
+            }
+
+            table[Function(workingString)].Delete(table[Function(workingString)].FindPosition(workingString));
+        }
+
+        /// <summary>
+        /// check: is there value in table
+        /// </summary>
+        public bool AreYouHere(string workingString)
+        {
+            return (table[Function(workingString)].IsThereValue(workingString));
+        }
+    } 
+}
---
+++ №2.3/№2.3Tests/HashTests.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _2._3;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._3.Tests
+{
+    [TestClass]
+    public class HashTests
+    {
+        private Hash hash;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            hash = new Hash();
+        }
+        
+        [TestMethod]
+        public void HashTest0()
+        {
+            hash.Add("I am Iron Man");
+            Assert.IsTrue(hash.AreYouHere("I am Iron Man"));
+        }
+
+        [TestMethod]
+        public void HashTest1()
+        {
+            hash.Add("Tu-u ru-u, tu ru ru. Tu-ru-tu-ru-tu-ru tu ru ru");
+            hash.Add("Tu-u ru-u, tu ru ru. Tu-ru-tu-ru-tu-ru tu ru ru");
+            hash.Add("Has he lost his mind?");
+            Assert.IsTrue(hash.AreYouHere("Tu-u ru-u, tu ru ru. Tu-ru-tu-ru-tu-ru tu ru ru"));
+            Assert.IsTrue(hash.AreYouHere("Has he lost his mind?"));
+        }
+
+        [TestMethod]
+        public void StrangeTestForHashTable2()
+        {
+            int result = hash.Function("Now the time is here");
+            Assert.AreEqual(result, hash.Function("Now the time is here"));
+        }
+
+        [TestMethod]
+        public void HashTest3()
+        {
+            hash.Add("Can he see or his mind?");
+            hash.Add("Can he walk at all");
+            hash.Add("Or if he moves will he fall?");
+            Assert.IsFalse(hash.AreYouHere("Is he alive or dead?"));
+        }
+
+        [TestMethod]
+        public void HashTest4()
+        {
+            hash.Add("Has he thoughts within his head");
+            hash.Add("We'll just pass him there");
+            hash.Add("Why should we even care?");
+            hash.Add("Why should we even care?");
+            hash.Delete("Why should we even care?");
+            hash.Delete("Why should we even care?");
+            Assert.IsFalse(hash.AreYouHere("Why should we even care?"));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ExceptionNonexistentPosition))]
+        public void HashTest5()
+        {
+            hash.Delete("He was turned to steel");
+        }
+    }
+}
