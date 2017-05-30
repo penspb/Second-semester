bda1730a6f7df4da639cfcf97c596a64d193ab5b
Ekaterina Petrova <penspb@gmail.com>
2017-05-31 00:05:32 +0300

    №5.2

---
+++ 5.2/5.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            EventLoop eventLoop = new EventLoop();
+            eventLoop.OnArrowUp += CursorMover.MoveUp;
+            eventLoop.OnArrowDown += CursorMover.MoveDown;
+            eventLoop.OnArrowLeft += CursorMover.MoveLeft;
+            eventLoop.OnArrowRight += CursorMover.MoveRight;
+
+            eventLoop.Run();
+        }
+    }
+}
